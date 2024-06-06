e320960076d2ed159985e46397bc46f6bea13795
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-06 17:11:45 -0500

    Solicitud a una API desde Go

---
+++ GitHub-Copilot-Curso/M1L3_API/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
+}
