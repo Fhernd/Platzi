fb5123d9ab0caceec420b1c776bde656791ef3aa
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-08 08:00:48 -0500

    Tipo de propiedad que menos ha estado presente en un documento

--- GitHub-Copilot-Curso/M3L2_Consultas/consultas.sql
+++ GitHub-Copilot-Curso/M3L2_Consultas/consultas.sql
@@ -1,4 +1,4 @@
--- ¿Cuáles son las propiedades que han vendido al menos dos veces?
+-- ¿Cuáles son las propiedades que se han vendido al menos dos veces?
 
 SELECT 
     p.id AS propiedad_id,
@@ -12,3 +12,22 @@ GROUP BY
     p.id, p.descripcion
 HAVING 
     COUNT(d.id) >= 2;
+
+
+-- ¿Entre vehículo e inmueble cuál de los dos es el que menos se ha visto presente en un documento de venta?
+SELECT 
+    tipo_propiedad.descripcion AS tipo,
+    COUNT(documento.id) AS cantidad_documentos
+FROM 
+    Documento documento
+JOIN 
+    Propiedad propiedad ON documento.propiedad_id = propiedad.id
+JOIN 
+    TipoPropiedad tipo_propiedad ON propiedad.tipo_propiedad_id = tipo_propiedad.id
+WHERE 
+    tipo_propiedad.descripcion IN ('Vehículo', 'Inmueble')
+GROUP BY 
+    tipo_propiedad.descripcion
+ORDER BY 
+    cantidad_documentos ASC
+LIMIT 1;
