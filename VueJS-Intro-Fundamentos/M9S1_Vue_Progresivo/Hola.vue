304b073dd9770de4e2124d304dd3a0026eb3508c
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-20 18:15:00 -0500

    Intentando ejecutar el archivo Hola.vue

---
+++ VueJS-Intro-Fundamentos/M9S1_Vue_Progresivo/Hola.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, excepturi cumque atque voluptatibus distinctio, delectus voluptates laudantium placeat incidunt voluptas vitae inventore quidem itaque! Eaque consectetur ut ipsa porro obcaecati.</p>
+        <br>
+
+        <p>{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'Hola Mundo'
+        }
+    },
+}
+</script>
+
+<style scoped>
+    p {
+        color: red;
+    }
+</style>
