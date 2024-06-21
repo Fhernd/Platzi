85fc9003e8cebe34e70f4d9d06682d9bb9f048c4
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-21 17:44:53 -0500

    Lógica de componentes

---
+++ VueJS-Intro-Fundamentos/M10S1_Proyecto/apps.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Register the ComparisonOption component
+    Vue.component('comparison-option', {
+        props: ['option', 'index'],
+        template: '#comparison-option-template'
+    });
+
+    // Register the Comparison component
+    Vue.component('comparison', {
+        props: ['comparison', 'index'],
+        template: '#comparison-template',
+        methods: {
+            removeOption(optIndex) {
+                this.$emit('remove-option', { comparisonIndex: this.index, optionIndex: optIndex });
+            }
+        }
+    });
+
+    // Create the main Vue instance
+    const app = new Vue({
+        el: '#app',
+        data: {
+            newComparison: {
+                title: '',
+                options: [
+                    { store: '', url: '', price: '', discount: false }
+                ]
+            },
+            comparisons: [],
+            editOption: { store: '', url: '', price: '', discount: false },
+            editIndices: { comparisonIndex: -1, optionIndex: -1 }
+        },
+        created() {
+            this.loadComparisons();
+        },
+        methods: {
+            addOption() {
+                this.newComparison.options.push({ store: '', url: '', price: '', discount: false });
+            },
+            removeOption({ comparisonIndex, optionIndex }) {
+                this.comparisons[comparisonIndex].options.splice(optionIndex, 1);
+                this.saveComparisons();
+            },
+            addComparison() {
+                this.comparisons.push({ ...this.newComparison });
+                this.saveComparisons();
+                this.resetForm();
+            },
+            resetForm() {
+                this.newComparison = { title: '', options: [{ store: '', url: '', price: '', discount: false }] };
+            },
+            saveComparisons() {
+                localStorage.setItem('comparisons', JSON.stringify(this.comparisons));
+            },
+            loadComparisons() {
+                const storedComparisons = localStorage.getItem('comparisons');
+                if (storedComparisons) {
+                    this.comparisons = JSON.parse(storedComparisons);
+                }
+            },
+            exportCSV() {
+                let csvContent = "data:text/csv;charset=utf-8,";
+                csvContent += "Título,Tienda,URL,Precio,Descuento\n";
+                this.comparisons.forEach(comparison => {
+                    comparison.options.forEach(option => {
+                        const discountText = option.discount ? 'Sí' : 'No';
+                        csvContent += `${comparison.title},${option.store},${option.url},${option.price},${discountText}\n`;
+                    });
+                });
+                const encodedUri = encodeURI(csvContent);
+                const link = document.createElement("a");
+                link.setAttribute("href", encodedUri);
+                link.setAttribute("download", "comparaciones.csv");
+                document.body.appendChild(link);
+                link.click();
+            },
+            deleteComparison(index) {
+                this.comparisons.splice(index, 1);
+                this.saveComparisons();
+            },
+            openEditModal({ comparisonIndex, optionIndex }) {
+                this.editIndices = { comparisonIndex, optionIndex };
+                this.editOption = { ...this.comparisons[comparisonIndex].options[optionIndex] };
+                new bootstrap.Modal(document.getElementById('editOptionModal')).show();
+            },
+            saveEdit() {
+                const { comparisonIndex, optionIndex } = this.editIndices;
+                this.comparisons[comparisonIndex].options[optionIndex] = { ...this.editOption };
+                this.saveComparisons();
+                bootstrap.Modal.getInstance(document.getElementById('editOptionModal')).hide();
+            }
+        }
+    });
+});
