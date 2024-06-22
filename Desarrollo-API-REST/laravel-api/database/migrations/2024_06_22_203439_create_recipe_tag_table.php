9300549aca24a32c87e00cc9f20e8975c3448e43
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-22 15:39:01 -0500

    Tabla recipe_tag como asociativa entre receta y etiqueta. Relación de muchos-a-muchos.

---
+++ Desarrollo-API-REST/laravel-api/database/migrations/2024_06_22_203439_create_recipe_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipe_tag', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('recipe_id');
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
+
+            $table->unsignedInteger('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipe_tag');
+    }
+};
