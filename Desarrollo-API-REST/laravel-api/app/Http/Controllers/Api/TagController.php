e591d19a60574e2f81c9dd952f271e9fe0d10792
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-25 17:07:27 -0500

    Métodos para el controlador de Etiquetas

--- Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/TagController.php
+++ Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/TagController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Tag;
+
 class TagController extends Controller
 {
-    //
+    public function index()
+    {
+        return Tag::all();
+    }
+
+    public function show(Tag $tag)
+    {
+        return $tag;
+    }
 }
