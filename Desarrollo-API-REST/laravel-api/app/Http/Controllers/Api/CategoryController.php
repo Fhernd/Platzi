a9f7fd66beba0879ace23957fff4622f318a77f9
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-25 13:56:42 -0500

    Rutas para el controlador de Categorías

--- Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/CategoryController.php
+++ Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/CategoryController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function show(Category $category)
+    {
+        return $category;
+    }
 }
