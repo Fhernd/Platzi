07f4c4be7ef519cecf30dd72ba8e2eee9f7e088b
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-25 13:55:27 -0500

    Mapeo de rutas de API

--- Desarrollo-API-REST/laravel-api/app/Providers/RouteServiceProvider.php
+++ Desarrollo-API-REST/laravel-api/app/Providers/RouteServiceProvider.php
@@ -37,4 +37,12 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
         });
     }
+
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
+    }
 }
