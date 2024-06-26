7f6577abfb1960f7c2c5f833d4111aff8c4e848c
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-26 17:53:02 -0500

    Traer los datos en las recetas o en una receta

--- Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/RecipeController.php
+++ Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/RecipeController.php
@@ -10,11 +10,11 @@ class RecipeController extends Controller
 {
     public function index()
     {
-        return Recipe::all();
+        return Recipe::with['category', 'tags', 'user']->get();
     }
 
     public function show(Recipe $recipe)
     {
-        return $recipe;
+        return $recipe->load('category', 'tags', 'user');
     }
 }
