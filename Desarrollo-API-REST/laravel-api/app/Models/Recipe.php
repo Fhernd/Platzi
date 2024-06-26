b55dd6736c8e9e69d0de996b90c275b1b23b8f47
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-26 17:53:55 -0500

    Las relaciones se hacen en el modelo

--- Desarrollo-API-REST/laravel-api/app/Models/Recipe.php
+++ Desarrollo-API-REST/laravel-api/app/Models/Recipe.php
@@ -13,4 +13,14 @@ class Recipe extends Model
     {
         return $this->belongsToMany(Tag::class);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
