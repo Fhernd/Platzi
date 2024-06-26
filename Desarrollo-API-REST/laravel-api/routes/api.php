a730e93ee066b84bfea81760bc7574903119451f
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-25 19:31:35 -0500

    Agregación de rutas como API

--- Desarrollo-API-REST/laravel-api/routes/api.php
+++ Desarrollo-API-REST/laravel-api/routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\RecipeController;
+
+
+Route::get('categories',            [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
+
+Route::get('recipes',            [RecipeController::class, 'index']);
+Route::get('recipes/{category}', [RecipeController::class, 'show']);
+
+Route::get('tags',            [TagController::class, 'index']);
+Route::get('tags/{category}', [TagController::class, 'show']);
