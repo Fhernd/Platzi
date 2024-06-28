b087617274f3a17f3d4c14c59894c667e50af6e7
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-28 06:45:13 -0500

    Conversión de un mapeo a un arreglo con toArray()

--- Desarrollo-API-REST/laravel-api/app/Http/Resources/CategoryCollection.php
+++ Desarrollo-API-REST/laravel-api/app/Http/Resources/CategoryCollection.php
@@ -22,6 +22,6 @@ class CategoryCollection extends ResourceCollection
                     'name' => $category->name,
                 ]
             ];
-        });
+        })->toArray();
     }
 }
