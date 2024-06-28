3f92a3ca186ab0d381e84044319ee42c0fe04d7e
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-28 06:43:22 -0500

    Uso de la transformación de categorías

--- Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/CategoryController.php
+++ Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/CategoryController.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Request;
 
 use App\Models\Category;
 
+use App\Http\Resources\CategoryCollection;
 use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return CategoryResource::collection(Category::all());
+        return new CategoryCollection(Category::all());
     }
 
     public function show(Category $category)
