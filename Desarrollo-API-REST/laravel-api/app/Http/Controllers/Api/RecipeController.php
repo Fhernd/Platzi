891e4009b9ea02f54005eca57a18deea4043daae
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-08 13:41:04 -0500

    Validación de solicitud de creación en clase especializada

--- Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/RecipeController.php
+++ Desarrollo-API-REST/laravel-api/app/Http/Controllers/Api/RecipeController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
+use App\Http\Requests\StoreRecipeRequest;
 
 
 class RecipeController extends Controller
@@ -25,23 +26,9 @@ class RecipeController extends Controller
         return new RecipeResource($recipe);
     }
 
-    public function store(Request $request)
+    public function store(StoreRecipeRequest $request)
     {
-        $request->validate([
-            'category_id' => 'required',
-            'user_id' => 'required',
-            'title' => 'required',
-            'description' => 'required',
-            'ingredients' => 'required',
-            'instructions' => 'required',
-            'image' => 'required',
-        ]);
-        
         $recipe = Recipe::create($request->all());
-
-        if ($tags = json_decode($request->tags)) {
-            $recipe->tags()->attach($tags);
-        }
         
         return response(new RecipeResource($recipe), Response::HTTP_CREATED);
     }
