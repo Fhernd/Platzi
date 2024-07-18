2f7b08d974c0822c657113a6219387158ab50efe
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-18 11:45:21 -0500

    Pruebas para la ruta tags

---
+++ Desarrollo-API-REST/laravel-api/tests/Feature/TagTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
+
+use App\Models\Tag;
+use App\Models\User;
+
+class TagTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $categories = Tag::factory(2)->create();
+
+        $response = $this->getJson('/api/tags')
+            ->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    ['id', 'name', 'type', 'attributes' => ['name'], 'relationships' => ['recipes' => []]]
+                ]
+            ]);
+    }
+
+    public function test_show(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $tag = Tag::factory()->create();
+
+        $response = $this->getJson('/api/tags/' . $tag->id)
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'data' => [
+                    'id', 'name', 'type', 'attributes' => ['name'], 'attributes' => ['name'], 'relationships' => ['recipes' => []]
+                ]
+            ]);
+    }
+}
