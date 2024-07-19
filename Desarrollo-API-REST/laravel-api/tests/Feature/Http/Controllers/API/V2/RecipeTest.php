45dc60320ed0bc54ffc2f83f256e2764fd8ded4b
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-19 13:32:46 -0500

    Pruebas de recetas en la versión 2 de la API

---
+++ Desarrollo-API-REST/laravel-api/tests/Feature/Http/Controllers/API/V2/RecipeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\API\V2;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
+
+use App\Models\Recipe;
+use App\Models\Category;
+use App\Models\User;
+
+class RecipeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+        Category::factory()->create();
+
+        $recipes = Recipe::factory(5)->create();
+
+        $response = $this->getJson('/api/v1/recipes')
+            ->assertJsonCount(5, 'data')
+            ->assertJsonStructure([
+                'data' => [],
+                'links' => [],
+                'meta' => [],
+            ]);
+    }
+}
